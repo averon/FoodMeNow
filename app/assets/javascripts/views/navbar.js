8dc46058261d2ab2f130c30de70c4f36f4dbcc6c
Rob Baldwin <rob.w.baldwin@gmail.com>
2014-09-27 13:27:39 -0700

    added styling to cart

--- app/assets/javascripts/views/navbar.js
+++ app/assets/javascripts/views/navbar.js
@@ -1,4 +1,5 @@
 FoodMeNow.Views.Navbar = Backbone.CompositeView.extend({
+  className: 'navbar-default navbar-fixed-top',
   template: JST['navbar'],
   render: function () {
     this.$el.html(this.template({ order: this.model.model }));
