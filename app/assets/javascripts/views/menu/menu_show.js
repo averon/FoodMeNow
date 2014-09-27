551f06d689ed8a4e22a8cec94bb242d8799f08e8
Rob Baldwin <rob.w.baldwin@gmail.com>
2014-09-26 18:21:33 -0700

    removed superfluous category index view

--- app/assets/javascripts/views/menu/menu_show.js
+++ app/assets/javascripts/views/menu/menu_show.js
@@ -9,12 +9,21 @@ FoodMeNow.Views.MenuShow = Backbone.CompositeView.extend({
   },
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
+    this.listenTo(this.model.menuCategories(), 'sync add', this.render);
+    this.listenTo(this.model.menuCategories(), 'add', this.addMenuCategory);
     
     var userOrder = new FoodMeNow.Views.OrderShow({ model: this.order() });
     this.addSubview('.user-order', userOrder.render()); 
 
-    var categoryIndex = new FoodMeNow.Views.MenuCategoryIndex({ model: this.model });
-    this.addSubview('.menu', categoryIndex.render());
+    var view = this;
+    var categories = this.model.menuCategories();
+    categories.each(function (category) {
+      view.addMenuCategory(category);
+    });
+  },
+  addMenuCategory: function (category) {
+    var categoryShow = new FoodMeNow.Views.MenuCategoryShow({ model: category });
+    this.addSubview('.menu', categoryShow.render());
   },
   order: function () {
     this._order = this._order || new FoodMeNow.Models.Order();
